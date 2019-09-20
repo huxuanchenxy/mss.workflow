d9a4ac96080dac54f5c3720ae850da49bfaaf07a
hyx <huyanxinag@outlook.com>
2019-09-20 14:05:11 +0800

    my apply base function

--- MSS.Platform.Workflow.WebApi/Data/WorkTaskRepo.cs
+++ MSS.Platform.Workflow.WebApi/Data/WorkTaskRepo.cs
@@ -90,7 +90,7 @@ namespace MSS.Platform.Workflow.WebApi.Data
 
                 var data = await c.QueryAsync<TaskViewModel>(sql.ToString());
                 var total = data.ToList().Count;
-                sql.Append(" order by t." + parm.sort + " " + parm.order)
+                sql.Append(" order by " + parm.sort + " " + parm.order)
                 .Append(" limit " + (parm.page - 1) * parm.rows + "," + parm.rows);
                 var ets = await c.QueryAsync<TaskViewModel>(sql.ToString());
 
@@ -112,56 +112,33 @@ namespace MSS.Platform.Workflow.WebApi.Data
             {
 
                 StringBuilder sql = new StringBuilder();
-                sql.Append($@" SELECT 
-                                t.ID,
-                                t.AppName,
-                                t.AppInstanceID,
-                                ai.ProcessGUID,
-                                pi.Version,
-                                t.ProcessInstanceID,
-                                ai.ActivityGUID,
-                                t.ActivityInstanceID,
-                                ai.ActivityName,
-                                ai.ActivityType,
-                                ai.WorkItemType,
-                                ai.CreatedByUserID,
-                                ai.CreatedByUserName,
-                                ai.CreatedDateTime,
-                                t.TaskType,
-                                t.EntrustedTaskID,
-                                t.AssignedToUserID,
-                                t.AssignedToUserName,
-                                t.CreatedDateTime,
-                                t.LastUpdatedDateTime,
-                                t.EndedDateTime,
-                                t.EndedByUserID,
-                                t.EndedByUserName,
-                                t.TaskState,
-                                ai.ActivityState,
-                                t.RecordStatusInvalid,
-                                pi.ProcessState,
-                                ai.ComplexType,
-                                ai.MIHostActivityInstanceID,
-                                pi.AppInstanceCode,
-                                pi.ProcessName,
-                                pi.CreatedByUserID,
-                                pi.CreatedByUserName,
-                                pi.CreatedDateTime,
-                                CASE WHEN ai.MIHostActivityInstanceID is null THEN ai.ActivityState ELSE ai1.ActivityState END MiHostState
-                            FROM
-                                WfActivityInstance ai
-                                    INNER JOIN
-                                WfTasks t ON ai.ID = t.ActivityInstanceID
-                                    INNER JOIN
-                                WfProcessInstance pi ON ai.ProcessInstanceID = pi.ID
-                                    LEFT JOIN
-                                WfActivityInstance ai1 ON ai.MIHostActivityInstanceID = ai1.ID ");
+                sql.Append($@"  SELECT pi.ID,
+		                                ai.AppName,
+		                                ai.ProcessGUID,
+		                                pi.Version,
+		                                ai.ActivityGUID,
+		                                ai.ActivityName,
+		                                ai.ActivityType,
+		                                ai.WorkItemType,
+		                                ai.ActivityState,
+		                                pi.ProcessState,
+		                                ai.ComplexType,
+		                                ai.MIHostActivityInstanceID,
+		                                pi.AppInstanceCode,
+		                                pi.ProcessName,
+		                                pi.CreatedByUserID,
+		                                pi.CreatedByUserName,
+		                                pi.CreatedDateTime
+                                FROM
+		                                WfActivityInstance ai
+                                INNER JOIN
+		                                WfProcessInstance pi ON ai.ProcessInstanceID = pi.ID ");
                 StringBuilder whereSql = new StringBuilder();
-                whereSql.Append(" WHERE pi.CreatedByUserID = '" + parm.AssignedToUserID + "' ");
+                whereSql.Append(" WHERE ai.ActivityType = '1' AND pi.CreatedByUserID = '" + parm.AssignedToUserID + "' ");
 
                 if (parm.AppName != null)
                 {
-                    whereSql.Append(" and t.AppName like '%" + parm.AppName.Trim() + "%'");
+                    whereSql.Append(" and ai.AppName like '%" + parm.AppName.Trim() + "%'");
                 }
 
                 sql.Append(whereSql);
@@ -169,7 +146,7 @@ namespace MSS.Platform.Workflow.WebApi.Data
 
                 var data = await c.QueryAsync<TaskViewModel>(sql.ToString());
                 var total = data.ToList().Count;
-                sql.Append(" order by t." + parm.sort + " " + parm.order)
+                sql.Append(" order by " + parm.sort + " " + parm.order)
                 .Append(" limit " + (parm.page - 1) * parm.rows + "," + parm.rows);
                 var ets = await c.QueryAsync<TaskViewModel>(sql.ToString());
 
